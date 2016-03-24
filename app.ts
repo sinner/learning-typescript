a9a3342ad78a6cf90e01631b0197e6c3195800b7
José Gabriel González <jgabrielsinner10@gmail.com>
2016-03-23 22:03:00 -0430

    Entendiendo el tipado de Typescript

--- app.ts
+++ app.ts
@@ -1,9 +1,44 @@
-class HelloWorld {
-    constructor(public message: string){
-        
-    }
+enum Category {Biography, Poetry, Fiction, History, Children};
+
+class Book {
+
+    constructor(public id: number, public title: string, public author: string, public available: boolean, public category: number) { }
+
 }
 
-var hello = new HelloWorld("Hello TypeScript");
-console.log(hello.message);
-console.log("Hello Visual Studio Code");
+class Library {
+
+    constructor(public id: number, public name: string) { }
+
+    public getAllBooks(): Array<Book> {
+        let books: Array<Book> = [
+            {id: 1, title: "Ulysses", author: "James Joyce", available: false, category: Category.Fiction},
+            {id: 2, title: "A Farewell To Arms", author: "Enest Hemingway", available: false, category: Category.Fiction},
+            {id: 3, title: "I Know Why The Caged Bird Sings", author: "Maya Angelou", available: true, category: Category.Poetry},
+            {id: 4, title: "Moby Dick", author: "Herman Melville", available: true, category: Category.Fiction}
+        ];
+        return books;
+    }
+
+    public logFirstAvilable(books: Array<Book>): void {
+
+        let numberOfBooks: number = books.length;
+        let firstAvalaible: string = "";
+
+        for (let currentBook of books) {
+
+            if (currentBook.available) {
+                firstAvalaible = currentBook.title;
+                break;
+            }
+        }
+
+        console.log("Total Books: " + numberOfBooks);
+        console.log("First Avalaible: " + firstAvalaible);
+    }
+
+}
+
+let myLibrary: Library = new Library(1, "Simón Bolívar");
+const allBooks: Array<Book> = myLibrary.getAllBooks();
+myLibrary.logFirstAvilable(allBooks);
--- js/app.js
+++ js/app.js
@@ -1,10 +1,52 @@
-var HelloWorld = (function () {
-    function HelloWorld(message) {
-        this.message = message;
+var Category;
+(function (Category) {
+    Category[Category["Biography"] = 0] = "Biography";
+    Category[Category["Poetry"] = 1] = "Poetry";
+    Category[Category["Fiction"] = 2] = "Fiction";
+    Category[Category["History"] = 3] = "History";
+    Category[Category["Children"] = 4] = "Children";
+})(Category || (Category = {}));
+;
+var Book = (function () {
+    function Book(id, title, author, available, category) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.available = available;
+        this.category = category;
     }
-    return HelloWorld;
+    return Book;
 }());
-var hello = new HelloWorld("Hello TypeScript");
-console.log(hello.message);
-console.log("Hello Visual Studio Code");
+var Library = (function () {
+    function Library(id, name) {
+        this.id = id;
+        this.name = name;
+    }
+    Library.prototype.getAllBooks = function () {
+        var books = [
+            { id: 1, title: "Ulysses", author: "James Joyce", available: false, category: Category.Fiction },
+            { id: 2, title: "A Farewell To Arms", author: "Enest Hemingway", available: false, category: Category.Fiction },
+            { id: 3, title: "I Know Why The Caged Bird Sings", author: "Maya Angelou", available: true, category: Category.Poetry },
+            { id: 4, title: "Moby Dick", author: "Herman Melville", available: true, category: Category.Fiction }
+        ];
+        return books;
+    };
+    Library.prototype.logFirstAvilable = function (books) {
+        var numberOfBooks = books.length;
+        var firstAvalaible = "";
+        for (var _i = 0, books_1 = books; _i < books_1.length; _i++) {
+            var currentBook = books_1[_i];
+            if (currentBook.available) {
+                firstAvalaible = currentBook.title;
+                break;
+            }
+        }
+        console.log("Total Books: " + numberOfBooks);
+        console.log("First Avalaible: " + firstAvalaible);
+    };
+    return Library;
+}());
+var myLibrary = new Library(1, "Simón Bolívar");
+var allBooks = myLibrary.getAllBooks();
+myLibrary.logFirstAvilable(allBooks);
 //# sourceMappingURL=app.js.map
